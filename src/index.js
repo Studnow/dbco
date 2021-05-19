e3de9a40e300e40bf1130e9df8f8c3301ad3dacc
Igor <silent659@gmail.com>
2021-05-19 03:31:42 +0300

    slider cards with rating and checkbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import './styles/main.sass'
+import './styles/main.sass';
 import Vue from 'vue';
+import Swiper from 'swiper'
+import "swiper/swiper-bundle.css";
+import SwiperCore, { Navigation } from 'swiper/core';
+
+SwiperCore.use([Navigation]);
+
+// swiper не работает если смонтирован экземпляр Vue, либо отключаем Vue, либо слайдер из Vue компонентов
+
+const slider = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
 
 window.Vue = require('vue')
 
-Vue.component('example-component', require('./components/Example.vue').default)
-Vue.component("aside-component", require("./components/aside.vue").default);
+// Vue.component('example-component', require('./components/Example.vue').default)
+// Vue.component("aside-component", require("./components/aside.vue").default);
 
-const app = new Vue({
-  el: '#app'
-})
+// const app = new Vue({
+//   el: '#app'
+// })
